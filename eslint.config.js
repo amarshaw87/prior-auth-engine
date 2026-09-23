b933a7f9bd2871593be103d6c1dda4f628acd122
Amår Shāw <amarshaw87@gmail.com>
2026-09-23 13:21:25 -0700

    Add ESLint configuration file
    Set up ESLint configuration for JavaScript and React hooks.

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        alert: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
+];
